2c6058b4129e1e38f5469202b8a65644c26bdc35
hernan <hernan.cely@nivelics.co>
2021-09-22 11:54:59 -0500

    se agrego validacion de email repetido

--- src/test/java/pom/RegisterPage.java
+++ src/test/java/pom/RegisterPage.java
@@ -7,9 +7,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.*;
+
 import java.time.Instant;
+import java.util.List;
 
 public class RegisterPage extends Base {
+
 
     By buttonRegister = By.cssSelector("a.psf-login__link--register");
     By nameLocator = By.id("sktab1_first_name");
@@ -22,6 +26,7 @@ public class RegisterPage extends Base {
     By countryBornLocator = By.id("sktab1_born");
     By selectYear = By.xpath("/html/body/div[16]/div/div/select/option[1]");
     By dateYearLocator = By.cssSelector("select.ui-datepicker-year");
+    By errorEmail = By.xpath("//*[@id=\"adver-5\"]/p[2]");
     //locators interest page
     By buttonNextPage = By.id("nextBtn");
     By interestPageLocator = By.xpath("/html/body/div[1]/div/div/div[1]/div/div[2]/article/form/div[2]/h2");
@@ -29,10 +34,17 @@ public class RegisterPage extends Base {
     String proyectSearchLocator = "busc_invetirP";
     String networkSearchLocator = "busc_networking";
     String mentoriySearchLocator = "bus_recibir";
-    By healthCategoryLocator = By.id("ctg_saludBienestar");
-    By cityCategoryLocator = By.id("ctg_ciudadesCo");
+    String HEALTH_CATEGORY_LOCATOR = "ctg_saludBienestar";
+    String CITY_CATEGORY_LOCATOR = "ctg_ciudadesCo";
+    String JUSTICE_CATEGORY_LOCATOR = "ctg_transparenciaJ";
     By justiceCategoryLocator = By.id("ctg_transparenciaJ");
     By searchLocator = By.xpath("//*[@id=\"bus_mentor\"]");
+    //locator accept terms
+    By button_register = By.id("nextBtn");
+    String CHECK_BOX_TERM = "check_AceptoT";
+    String CHECK_BOX_P = "check_AceptoP";
+    String CHECK_BOX_Z = "check_autoriz";
+
 
     public RegisterPage(WebDriver webDriver) {
         super(webDriver);
@@ -49,13 +61,21 @@ public class RegisterPage extends Base {
             typeInFields("sibate", cityLocator);
             typeInFields("test@mail.com", emailLocator);
             typeInFields("ct12345**", passwordLocator);
-            verifyDate();
-            typeInFields("colombia", countryBornLocator);
-            interestPage();
+            Thread.sleep(2000);
+            if (isDisplayed(errorEmail)){
+                System.out.println("ingrese un correo diferente");
+            }else{
+                verifyDate();
+                typeInFields("colombia", countryBornLocator);
+                click(buttonNextPage);
 
+                interestPage();
+            }
         } else {
             System.out.println("No registered");
         }
+
+
     }
 
     public void verifyDate() {
@@ -70,7 +90,6 @@ public class RegisterPage extends Base {
 //test interest page
 
     public void interestPage() throws InterruptedException {
-        click(buttonNextPage);
         Thread.sleep(2000);
         if (isDisplayed(interestPageLocator)) {
             //useVerticalScrolBar(180);
@@ -78,6 +97,8 @@ public class RegisterPage extends Base {
             click(profileLocator);
             selectMentory();
             selectCategory();
+            acceptTerms();
+
         } else {
             System.out.println("page not found");
         }
@@ -92,9 +113,19 @@ public class RegisterPage extends Base {
 
     public void selectCategory() throws InterruptedException {
         tab(justiceCategoryLocator);
-        selectCheckBox("ctg_impactoMedio");
-        selectCheckBox("ctg_saludBienestar");
-        selectCheckBox("ctg_transparenciaJ");
+        selectCheckBox(JUSTICE_CATEGORY_LOCATOR);
+        selectCheckBox(HEALTH_CATEGORY_LOCATOR);
+        selectCheckBox(CITY_CATEGORY_LOCATOR);
+    }
+
+    public void acceptTerms() throws InterruptedException {
+        tab(button_register);
+
+        selectCheckBox(CHECK_BOX_TERM);
+        selectCheckBox(CHECK_BOX_P);
+        selectCheckBox(CHECK_BOX_Z);
+        Thread.sleep(500);
+        click(button_register);
     }
 
 
